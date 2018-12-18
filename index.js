15aa1ea881a9608cd69561e997f33f01a1892f1a
cwcarlsen <christopher@wrem.co>
2018-12-18 17:12:55 +0100

    Callback route

--- index.js
+++ index.js
@@ -34,6 +34,49 @@ app.get('/shopify', (req, res) => {
   }
 });
 
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    // DONE: Validate request is from Shopify
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+        'utf-8'
+      );
+    let hashEquals = false;
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    res.status(200).send('HMAC validated');
+    // TODO
+    // Exchange temporary code for a permanent access token
+      // Use access token to make API call to 'shop' endpoint
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log('Example app listening on port 3000!');
 });
