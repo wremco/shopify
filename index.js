25fe6831be8b8fae831630d8b9772801d5550f6e
cwcarlsen <christopher@wrem.co>
2018-12-18 17:25:53 +0100

    Stop data

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.get('/shopify/callback', (req, res) => {
 
       request.get(shopRequestUrl, { headers: shopRequestHeaders })
       .then((shopResponse) => {
-        res.status(200).end(shopResponse);
+        //res.status(200).end(shopResponse);
       })
       .catch((error) => {
         res.status(error.statusCode).send(error.error.error_description);
